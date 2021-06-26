2805571d7f682da683d9d7593f32ae9da6968a03
dudesahn <dudesahn@gmail.com>
2021-06-26 16:15:21 -0400

    fix: add new test

---
+++ tests/test_old_harvest.py
@@ -0,0 +1,44 @@
+import brownie
+from brownie import Contract
+from brownie import config
+
+# test passes as of 21-05-20
+def test_simple_harvest(gov, token, vault, dudesahn, strategist, whale, strategy, chain, strategist_ms, staking, rewardsContract):
+    ## deposit to the vault after approving
+    startingWhale = token.balanceOf(whale)
+    token.approve(vault, 2 ** 256 - 1, {"from": whale})
+    vault.deposit(1000e18, {"from": whale})
+    newWhale = token.balanceOf(whale)
+    
+    # harvest, store asset amount
+    strategy.harvest({"from": gov})
+    old_assets_dai = vault.totalAssets()
+    assert old_assets_dai > 0
+    assert token.balanceOf(strategy) == 0
+    assert strategy.estimatedTotalAssets() > 0
+    assert staking.balanceOf(strategy, token) > 0
+    print("\nStarting Assets: ", old_assets_dai/1e18)
+    print("\nAssets Staked: ", staking.balanceOf(strategy, token)/1e18)
+        
+    # simulate 9 days of earnings
+    chain.sleep(86400*9)
+    chain.mine(1)
+
+    # harvest after a day, store new asset amount
+    strategy.harvest({"from": gov})
+    new_assets_dai = vault.totalAssets()
+    # we can't use strategyEstimated Assets because the profits are sent to the vault
+    assert new_assets_dai >= old_assets_dai
+    print("\nAssets after 2 days: ", new_assets_dai/1e18)
+
+
+    # Display estimated APR based on the two days before the pay out
+    print("\nEstimated SUSHI APR: ", "{:.2%}".format(((new_assets_dai - old_assets_dai) * (365 / 9)) / (strategy.estimatedTotalAssets())))
+    
+    # simulate a day of waiting for share price to bump back up
+    chain.sleep(86400)
+    chain.mine(1)
+    
+    # withdraw and confirm we made money
+    vault.withdraw({"from": whale})    
+    assert token.balanceOf(whale) >= startingWhale
