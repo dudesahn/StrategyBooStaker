ffb46fa19d962b72d4be4ee8ec10a31d147779ee
dudesahn <dudesahn@gmail.com>
2021-10-20 23:20:44 -0400

    feat: add all possible contracts for testing options

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,21 +12,49 @@ def isolation(fn_isolation):
 def token():  # this should be the address of the ERC-20 used by the strategy/vault. In this case, SUSHI
     token_address = "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2"
     yield Contract(token_address)
+    # SUSHI 0x6B3595068778DD592e39A122f4f5a5cF09C90fE2
+    # LINK 0x514910771AF9Ca656af840dff83E8264EcF986CA
+    # SNX 0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F
+    # XYZ 0x618679dF9EfCd19694BB1daa8D00718Eacfa2883
+    # ILV 0x767FE9EDC9E0dF98E07454847909b5E959D7ca0E
+    # BOND 0x0391D2021f89DC339F60Fff84546EA23E337750f
+    # MANA 0x0F5D2fB29fb7d3CFeE444a200298f468908cC942
 
 
 @pytest.fixture(scope="module")
 def emissionToken():  # this is the token we receive to sell for more want
-    yield Contract("0x7b39917f9562C8Bc83c7a6c2950FF571375D505D")
+    yield Contract("0xd779eEA9936B4e323cDdff2529eb6F13d0A4d66e")
+    # league 0x7b39917f9562C8Bc83c7a6c2950FF571375D505D
+    # enter 0xd779eEA9936B4e323cDdff2529eb6F13d0A4d66e
 
 
 @pytest.fixture(scope="module")
 def rewardscontract():  # this is the individual farming contract for our want
-    yield Contract("0xcCB04b5AD3eE27a1c3fe8eA15CaC802aD4F4f5B8")
+    yield Contract("0xA9B4B5e05EF90cFb8644591186AbCf883eDA5735")
+    # league
+    # sushi 0xcCB04b5AD3eE27a1c3fe8eA15CaC802aD4F4f5B8
+    # ilv 0x0636aAdbcfC05cEfAb81E80c5E5a1f49de2053A8
+    # link 0xBc1ce49430073A3154beCd8A090E74845Cd835fC
+    # snx 0xd4E9821F54cAf6019500252F88158f4D8f469922
+    # sushi 0xcCB04b5AD3eE27a1c3fe8eA15CaC802aD4F4f5B8
+    # bond 0x0Ee09C6F99b2D27D8Ca712033370647F347E32ba
+    # xyz 0x11CF7938CDF238c70b406E4650CFfF40614107EE
+
+    # enter
+    # sushi 0xA9B4B5e05EF90cFb8644591186AbCf883eDA5735
+    # hidden snx 0xfCeD219D83dFf48ee702dab93122de8d2aff314f
+    # axs 0xEa0B66eEc998Bb559fA6f1c6b159828F0a2db09d
+    # ilv 0x271DBa83ccADd6A9719e314F53D9BF4e2C79d21A
+    # xyz 0xcBA851105bFa485E390FBD456B9190824aE1212a
+    # mana 0x9b11E10f54010A52DdFAAF2E37C11EF9891a59F0
+    # bond 0xd65483B8755aC5Fea7120e3f68Df961d88181Dd2
 
 
 @pytest.fixture(scope="module")
 def staking():  # this is the staking contract, will be the same for each pool in the same protocol
-    yield Contract("0x43921eb2E5C78D9e887d3Ecd4620a3Bd606f4F95")
+    yield Contract("0x3F148612315AaE2514AC630D6FAf0D94B8Cd8E33")
+    # leagueDAO 0x43921eb2E5C78D9e887d3Ecd4620a3Bd606f4F95
+    # enterDAO 0x3F148612315AaE2514AC630D6FAf0D94B8Cd8E33
 
 
 # this is the amount of funds we have our whale deposit. adjust this as needed based on their wallet balance. Make sure to do no more than half of their balance.
@@ -39,7 +67,7 @@ def amount():
 # this is the name we want to give our strategy
 @pytest.fixture(scope="module")
 def strategy_name():
-    strategy_name = "StrategyLeagueSUSHI"
+    strategy_name = "StrategyEnterSUSHI"
     yield strategy_name
 
 
